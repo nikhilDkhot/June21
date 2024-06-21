a8d3189338930c758c4f4d24717fc8c53fd3f340
nikhilDkhot <nikhillkhot369@gmail.com>
2024-06-21 14:18:48 +0530

    2

---
+++ Datatype.js
@@ -0,0 +1,36 @@
+let num = 2;
+let num2 = 11.333;
+let num3 = Infinity;
+let num4 = 'some'/2;
+console.log(num)
+console.log(num2)
+console.log(num3)
+console.log(num4)
+
+console.log("---------------")
+
+let str = "Hello";
+let str1 = 'be aware';
+let phrase = `in present ${str1}`;
+
+console.log(str)
+console.log(str1)
+console.log(phrase)
+
+console.log("---------------")
+let loggedin = true
+let loggedout = false
+console.log(loggedin)
+console.log(loggedout)
+
+console.log("---------------")
+let wisdom = null
+console.log(wisdom)
+
+console.log("---------------")
+let bigInt = BigInt(5555555555)
+console.log(bigInt)
+
+
+
+
---
+++ Variable.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+//SyntaxError: Missing initializer in const declaration
+//carName = "Volvo";
+//const carName;
+
+//ReferenceError: Cannot access 'carName' before initialization
+//carName = "Volvo";
+//let carName;
+
+carName = "volvo"
+var carName;
+console.log(carName)
+
