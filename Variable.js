536e30ec713898154bdc3052f841f8794b999463
nikhilDkhot <nikhillkhot369@gmail.com>
2024-06-21 15:57:54 +0530

    4

--- Variable.js
+++ Variable.js
@@ -1,3 +1,36 @@
+//Declaring a JavaScript Variable
+var x;
+let y;
+
+//assign 
+x="nikhil"
+y="sohan"
+
+//One Statement, Many Variables
+let a=54,b=89,c="new";
+
+//undefined
+ let n;
+ console.log(n)
+
+ //You cannot re-declare a variable declared with let or const.
+let m =5
+//let m;  error
+
+let num = 5 + 2 + 3;
+
+let text ="mm"+" "+"jj"
+
+let nutext ="5"+ 2 + 5;
+//If you put a number in quotes, the rest of the numbers will be treated as strings, and concatenated.
+
+let nutext1 = 2 + 3 + "5";
+
+console.log(nutext1)
+console.log(num)
+
+
+
 
 
 
