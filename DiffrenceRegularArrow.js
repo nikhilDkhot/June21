da476cfbba62217ca45fae35ee9f8c249be810d6
nikhilDkhot <nikhillkhot369@gmail.com>
2024-06-22 14:33:21 +0530

    function arrow&regular

--- DiffrenceRegularArrow.js
+++ DiffrenceRegularArrow.js
@@ -12,5 +12,37 @@ const obj = {
     }
 };
 
-obj.regularFunction(); // undefined (or error in strict mode)
-obj.arrowFunction();   // 42
+//obj.regularFunction(); // undefined (or error in strict mode)
+//obj.arrowFunction();   // 42
+
+//Arrow functions do not have their own arguments object. They inherit arguments from the enclosing scope.
+function regularFunction() {
+    console.log(arguments); // [1, 2, 3]
+}
+
+const arrowFunction = () => {
+    console.log(arguments); // ReferenceError: arguments is not defined
+};
+
+regularFunction(1, 2, 3);
+//arrowFunction(1, 2, 3); // Error
+
+//To use arguments in an arrow function, you can use rest parameters
+const arrowFunction1 = (...args) => {
+    console.log(args); // [1, 2, 3]
+};
+arrowFunction1(1, 2, 3);
+
+
+//Arrow functions cannot be used as constructors and will throw an error if used with the new keyword.
+const ArrowConst = () => {
+    this.value = 42;
+};
+//new ArrowConst(); // TypeError: ArrowConstructor is not a constructor
+
+
+
+
+
+
+
