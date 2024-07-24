3de421070e95885be6c0792c2523fb2b5cbdf453
nikhilDkhot <nikhillkhot369@gmail.com>
2024-07-24 10:39:05 +0530

    [nikhil]added  database query

--- Generic.ts
+++ Generic.ts
@@ -3,3 +3,26 @@ function createPair<S, T>(v1: S, v2: T): [S,T] {
   }
   console.log(createPair<string, number>('hello', 42)); // ['hello', 42]
   console.log(createPair<number, number>(33, 33));
+
+/*****************************/
+class NamedValue<T> {
+    private _value: T | undefined;
+  
+    constructor(private name: string) {}
+  
+    public setValue(value: T) {
+      this._value = value;
+    }
+  
+    public getValue(): T | undefined {
+      return this._value;
+    }
+  
+    public toString(): string {
+      return `${this.name}: ${this._value}`;
+    }
+  }
+  
+  let value = new NamedValue<number>('myNumber');
+  value.setValue(10);
+  console.log(value.toString()); // myNumber: 10
